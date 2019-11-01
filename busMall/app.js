4a5c900839fe7c855ba239b3ac8fa04cc35f2353
Austin Smith <austinleesmith97@gmail.com>
2019-11-01 15:51:33 -0700

    table

--- busMall/app.js
+++ busMall/app.js
@@ -1,6 +1,5 @@
 'use strict';
-var totalClick= 0;
-// allProducts.tallyEl = document.getElementById('tally');
+
 var leftImageEl = document.getElementById('left');
 var centerImageEl = document.getElementById('center');
 var rightImageEl = document.getElementById('right');
@@ -44,32 +43,31 @@ function renderProducts(){
   while (uniquePicsArray[0] === uniquePicsArray[1]){
     uniquePicsArray[1] = makeRandom();
   }
-  while(uniquePicsArray[0] === uniquePicsArray[2]){
-    uniquePicsArray[2] = makeRandom();
-  }
   while (uniquePicsArray[1] === uniquePicsArray[2]){
     uniquePicsArray[2] = makeRandom();
   }
+  while (uniquePicsArray[0] === uniquePicsArray[2]){
+    uniquePicsArray[2] = makeRandom();
+  }
 
 
-  allProducts[uniquePicsArray[0]].views++;
+  allProducts[uniquePicsArray[0]].views++ ;
   leftImageEl.src = allProducts[uniquePicsArray[0]].path;
   leftImageEl.title = allProducts[uniquePicsArray[0]].name;
   leftImageEl.name = allProducts[uniquePicsArray[0]].name;
 
 
 
-  allProducts[uniquePicsArray[1]].views++;
+  allProducts[uniquePicsArray[1]].views++ ;
   centerImageEl.src = allProducts[uniquePicsArray[1]].path;
   centerImageEl.title = allProducts[uniquePicsArray[1]].name;
   centerImageEl.name = allProducts[uniquePicsArray[1]].name;
 
-  allProducts[uniquePicsArray[2]].views++;
+  allProducts[uniquePicsArray[2]].views++ ;
   rightImageEl.src = allProducts[uniquePicsArray[2]].path;
   rightImageEl.title = allProducts[uniquePicsArray[2]].name;
   rightImageEl.name = allProducts[uniquePicsArray[2]].name;
 }
-
 new Product('bag');
 new Product('banana');
 new Product('bathroom');
@@ -77,7 +75,6 @@ new Product('boots');
 new Product('breakfast');
 new Product('bubblegum');
 new Product('chair');
-
 
 
 
@@ -90,59 +87,16 @@ function handleClick(){
     if(allProducts[i].name === chosenImage){
       allProducts[i].clicks++;
     }
-    if(totalClick === 24){
-      containerEl.removeEventListener('click', handleClick, true);
-      containerEl.remove();
-    }
-
-    renderProducts();
-    parentEl.innerHTML= '';
-    render();
 
   }
-  totalClick++;
+
+
+  renderProducts();
 }
-
-
-
 
 containerEl.addEventListener('click', handleClick, true);
 
 
+
+
 renderProducts();
-// allProducts.nameData = [];
-// allProducts.viewsData = [];
-// allProducts.clicksData = [];
-// var getChartData = function(){
-//   for (var i = 0; i< allProducts.length; i++){
-//     allProducts.nameData.push(allProducts[i].name);
-//     allProducts.viewsData.push(allProducts[i].views);
-//     allProducts.clicksData.push(allProducts[i].clicks);
-//   }
-// }
-// allProducts.data = {
-//   labels: allProducts.nameData,
-//   datasets:[{
-//     fillColor:
-
-//   }
-//   ]
-
-// }
-var parentEl = document.getElementById('parentElement');
-
-var child = document.createElement('h1');
-child.textContent = 'Data: ';
-parentEl.appendChild(child);
-function render() {
-  for( var i = 0; i < allProducts.length; i++ ) {
-    var childEl = document.createElement('li');
-    childEl.textContent = `Product...${allProducts[i].name}     Views... ${allProducts[i].views}     Popularity...${allProducts[i].clicks}`;
-    parentEl.appendChild(childEl);
-  }
-
-
-}
-
-render();
-
